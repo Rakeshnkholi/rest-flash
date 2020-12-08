0f1c571530e925249e030a2b4a227c0ffaf8fd87
rakesh <rakesh@DESKTOP-6581DCU>
2020-12-08 20:17:24 +0530

    caliculator is created

---
+++ RestFramework/src/test/java/Testcases/Caliculator.java
@@ -0,0 +1,17 @@
+package Testcases;
+
+public class Caliculator
+{
+	public static int add(int a , int b)
+	{
+		int c= a+b;
+		return c;
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(add(5,10));
+		
+	}
+
+}
